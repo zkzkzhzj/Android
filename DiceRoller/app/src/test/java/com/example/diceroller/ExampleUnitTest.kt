b49d669e0df9f4140217c921d3bcc20faf89b840
zkzkzhzj <zkzkzhzj@gmail.com>
2022-08-08 23:09:34 +0900

    success DiceRoller App

--- DiceRoller/app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ DiceRoller/app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.example.diceroller
 
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +9,12 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number(){
+        val dice = Dice(6)
+        val rollResult = dice.roll()
+
+        assertTrue("값은 항상 1과 6사이로 나와야한다", rollResult in 1..6)
     }
 }
