a42e24dd84cab8a48c7af873970ccae5229f0a99
zkzkzhzj <zkzkzhzj@gmail.com>
2023-05-23 01:38:24 +0900

    feat: viewModel 내부에서 사용

--- Android_Basics_in_Kotlin/Unit6/android-workmanager-start_kotlin/app/src/main/java/com/example/background/BlurViewModel.kt
+++ Android_Basics_in_Kotlin/Unit6/android-workmanager-start_kotlin/app/src/main/java/com/example/background/BlurViewModel.kt
@@ -22,9 +22,12 @@ import android.content.Context
 import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.example.background.workers.BlurWorker
 
 class BlurViewModel(application: Application) : ViewModel() {
+    private val workManager = WorkManager.getInstance(application)
 
     internal var imageUri: Uri? = null
     internal var outputUri: Uri? = null
@@ -32,11 +35,14 @@ class BlurViewModel(application: Application) : ViewModel() {
     init {
         imageUri = getImageUri(application.applicationContext)
     }
+
     /**
      * Create the WorkRequest to apply the blur and save the resulting image
      * @param blurLevel The amount to blur the image
      */
-    internal fun applyBlur(blurLevel: Int) {}
+    internal fun applyBlur(blurLevel: Int) {
+        workManager.enqueue(OneTimeWorkRequest.from(BlurWorker::class.java))
+    }
 
     private fun uriOrNull(uriString: String?): Uri? {
         return if (!uriString.isNullOrEmpty()) {
